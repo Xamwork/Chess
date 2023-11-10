3e812f33aa6090751e3b44c584c9f38051faa626
Dmitrii Bedenchuk <xamwork21@gmail.com>
2023-11-10 11:56:39 +0400

    Добавлен новый класс Пешка

---
+++ src/main/java/org/example/Pawn.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class Pawn extends ChessPiece{
+
+    public Pawn(String color) {
+        super(color);
+    }
+    public String getColor() {
+        return super.color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn)) {
+            if (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].getColor().equals(this.getColor())) {
+                if (this.getColor().equals("White")) {
+                    if (line == 1) {
+                        if (toLine - line == 1 || toLine - line == 2) {
+                            if (toColumn == column) {
+                                if (toLine - line == 2) {
+                                    if (chessBoard.board[line + 1][column] == null) {
+                                        return true;
+                                    } else return false;
+                                } else return true;
+                            } else return false;
+                        } else return false;
+                    } else {
+                        if (toLine - line == 1) {
+                            if (toColumn == column) {
+                                return true;
+                            } else return false;
+                        } else return false;
+                    }
+                } else {
+                    if (line == 6) {
+                        if (line - toLine == 1 || line - toLine == 2) {
+                            if (toColumn == column) {
+                                if (line - toLine == 2) {
+                                    if (chessBoard.board[line - 1][column] == null) {
+                                        return true;
+                                    } else return false;
+                                } else return true;
+                            } else return false;
+                        } else return false;
+                    } else {
+                        if (line - toLine == 1) {
+                            if (toColumn == column) {
+                                return true;
+                            } else return false;
+                        } else return false;
+                    }
+                }
+            } else return false;
+        } else return false;
+        }
+
+    public String getSymbol() {
+        return "P";
+    }
+}
